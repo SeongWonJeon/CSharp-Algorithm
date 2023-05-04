973192316447de97256a5dcb32037776dc70a5d5
SeongWon Jeon <gateoper1105@gmail.com>
2023-05-04 21:45:50 +0900

    docs : TextRPG 구현방법
    텍스트RPG를 구현하기위해 어떻게 해야하는가 방법작성

---
+++ project TextRPG/Item/Item.cs
@@ -0,0 +1,20 @@
+﻿using project_TextRPG;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_TextRPG
+{
+    public abstract class Item
+    {
+        public string name;
+        public string description;
+        public char icon = '★';
+        public Position pos;
+        public int weight;
+
+        public abstract void Use();
+    }
+}
